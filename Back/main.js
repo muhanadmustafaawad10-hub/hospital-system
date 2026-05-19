52427a47cfa9e29f4783efa7e9283f678dd4dda4
Mohanad <muhanadmustafaawad10@gmail.com>
2026-05-19 08:39:09 +0300

    add start script

--- Back/main.js
+++ Back/main.js
@@ -2,13 +2,13 @@ const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
 const cors = require("cors")
-app.use(cors())
+app.use(cors({ origin: "*" }))
 app.use(express.json())
 const DoctorsRouter = require("./Routers/DoctorsRouter")
 const PatientsRouter = require("./Routers/PatientsRouter")
 const AppointRouter = require("./Routers/AppointsRouter")
 const registerRouter=require("./Routers/RegisterRouter")
-app.listen(4000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log("yes ilistend in 4000");
 })
 
